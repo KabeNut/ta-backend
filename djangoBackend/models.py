4925bd8678e9fe529c603fe2c910dbd32a22380c
Setyo Aryo <setyoaryopangestu25@gmail.com>
2024-04-14 00:27:42 +0700

    refactor: code cleaning

--- djangoBackend/models.py
+++ djangoBackend/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 
+
 class Patient(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
@@ -13,6 +14,7 @@ class Patient(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class VitalData(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -28,72 +30,43 @@ class VitalData(models.Model):
     deleted_at = models.DateTimeField(null=True, blank=True, editable=False)
 
     def calculate_ews_score(self):
-        pulse_model = [
-            CentileModel.objects.filter(vital_model__name='Pulse').latest("created_at").first_percentile,
-            CentileModel.objects.filter(vital_model__name='Pulse').latest("created_at").fifth_percentile,
-            CentileModel.objects.filter(vital_model__name='Pulse').latest("created_at").tenth_percentile,
-            CentileModel.objects.filter(vital_model__name='Pulse').latest("created_at").ninetieth_percentile,
-            CentileModel.objects.filter(vital_model__name='Pulse').latest("created_at").ninetyfifth_percentile,
-            CentileModel.objects.filter(vital_model__name='Pulse').latest("created_at").ninetyninth_percentile
-        ]
-        
-        sys_model = [
-            CentileModel.objects.filter(vital_model__name='Systol').latest("created_at").first_percentile,
-            CentileModel.objects.filter(vital_model__name='Systol').latest("created_at").fifth_percentile,
-            CentileModel.objects.filter(vital_model__name='Systol').latest("created_at").tenth_percentile,
-            CentileModel.objects.filter(vital_model__name='Systol').latest("created_at").ninetieth_percentile,
-            CentileModel.objects.filter(vital_model__name='Systol').latest("created_at").ninetyfifth_percentile,
-            CentileModel.objects.filter(vital_model__name='Systol').latest("created_at").ninetyninth_percentile
-        ]
-        
-        dia_model = [
-            CentileModel.objects.filter(vital_model__name='Diastol').latest("created_at").first_percentile,
-            CentileModel.objects.filter(vital_model__name='Diastol').latest("created_at").fifth_percentile,
-            CentileModel.objects.filter(vital_model__name='Diastol').latest("created_at").tenth_percentile,
-            CentileModel.objects.filter(vital_model__name='Diastol').latest("created_at").ninetieth_percentile,
-            CentileModel.objects.filter(vital_model__name='Diastol').latest("created_at").ninetyfifth_percentile,
-            CentileModel.objects.filter(vital_model__name='Diastol').latest("created_at").ninetyninth_percentile
-        ]
-        
-        rr_model = [
-            CentileModel.objects.filter(vital_model__name='Respiration').latest("created_at").first_percentile,
-            CentileModel.objects.filter(vital_model__name='Respiration').latest("created_at").fifth_percentile,
-            CentileModel.objects.filter(vital_model__name='Respiration').latest("created_at").tenth_percentile,
-            CentileModel.objects.filter(vital_model__name='Respiration').latest("created_at").ninetieth_percentile,
-            CentileModel.objects.filter(vital_model__name='Respiration').latest("created_at").ninetyfifth_percentile,
-            CentileModel.objects.filter(vital_model__name='Respiration').latest("created_at").ninetyninth_percentile
-        ]
-        
-        o2_model = [
-            CentileModel.objects.filter(vital_model__name='Oxygen').latest("created_at").first_percentile,
-            CentileModel.objects.filter(vital_model__name='Oxygen').latest("created_at").fifth_percentile,
-            CentileModel.objects.filter(vital_model__name='Oxygen').latest("created_at").tenth_percentile,
-            CentileModel.objects.filter(vital_model__name='Oxygen').latest("created_at").ninetieth_percentile,
-            CentileModel.objects.filter(vital_model__name='Oxygen').latest("created_at").ninetyfifth_percentile,
-            CentileModel.objects.filter(vital_model__name='Oxygen').latest("created_at").ninetyninth_percentile
-        ]
-        
-        def clustering(vital_data, percentile_model):
-            if vital_data < percentile_model[0] or vital_data > percentile_model[5]:
-                return 3
-            elif vital_data > percentile_model[0] and vital_data < percentile_model[1]:
-                return 2
-            elif vital_data > percentile_model[1] and vital_data < percentile_model[2]:
-                return 1
-            elif vital_data > percentile_model[3] and vital_data < percentile_model[4]:
-                return 1
-            elif vital_data > percentile_model[4] and vital_data < percentile_model[5]:
-                return 2
-            else:
-                return 0
+        pulse_model = self.get_latest_percentiles("Pulse")
+        sys_model = self.get_latest_percentiles("Systol")
+        dia_model = self.get_latest_percentiles("Diastol")
+        rr_model = self.get_latest_percentiles("Respiration")
+        o2_model = self.get_latest_percentiles("Oxygen")
 
-        pulse_score = clustering(self.pulse, pulse_model)
-        sys_score = clustering(self.systol, sys_model)
-        dia_score = clustering(self.diastol, dia_model)
-        rr_score = clustering(self.respiration_rate, rr_model)
-        o2_score = clustering(self.oxygen_saturation, o2_model)
+        pulse_score = self.clustering(self.pulse, pulse_model)
+        sys_score = self.clustering(self.systol, sys_model)
+        dia_score = self.clustering(self.diastol, dia_model)
+        rr_score = self.clustering(self.respiration_rate, rr_model)
+        o2_score = self.clustering(self.oxygen_saturation, o2_model)
         ews_score = pulse_score + sys_score + dia_score + rr_score + o2_score
         return ews_score / 5
+
+    def clustering(self, vital_data, percentile_model):
+        if vital_data < percentile_model[0] or vital_data > percentile_model[5]:
+            return 3
+        elif vital_data > percentile_model[0] and vital_data < percentile_model[1]:
+            return 2
+        elif vital_data > percentile_model[1] and vital_data < percentile_model[2]:
+            return 1
+        elif vital_data > percentile_model[3] and vital_data < percentile_model[4]:
+            return 1
+        elif vital_data > percentile_model[4] and vital_data < percentile_model[5]:
+            return 2
+        else:
+            return 0
+
+    def get_latest_percentiles(self, vital_model_name):
+        return [
+            CentileModel.objects.filter(vital_model__name=vital_model_name).latest("created_at").first_percentile,
+            CentileModel.objects.filter(vital_model__name=vital_model_name).latest("created_at").fifth_percentile,
+            CentileModel.objects.filter(vital_model__name=vital_model_name).latest("created_at").tenth_percentile,
+            CentileModel.objects.filter(vital_model__name=vital_model_name).latest("created_at").ninetieth_percentile,
+            CentileModel.objects.filter(vital_model__name=vital_model_name).latest("created_at").ninetyfifth_percentile,
+            CentileModel.objects.filter(vital_model__name=vital_model_name).latest("created_at").ninetyninth_percentile,
+        ]
 
     def save(self, *args, **kwargs):
         self.ews_score = self.calculate_ews_score()
@@ -102,16 +75,18 @@ class VitalData(models.Model):
     def update(self, *args, **kwargs):
         self.ews_score = self.calculate_ews_score()
         super(VitalData, self).save(*args, **kwargs)
-    
+
     def __str__(self):
         return self.patient.name
+
 
 class VitalModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    
+
     def __str__(self):
         return self.name
+
 
 class CentileModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -123,6 +98,6 @@ class CentileModel(models.Model):
     ninetyfifth_percentile = models.FloatField()
     ninetyninth_percentile = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.vital_model.name
